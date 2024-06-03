71746ec7dc4e4305288117f9b475ec267c6535de
Dariusz Okoński <darek200180@gmail.com>
2024-06-03 20:57:31 +0200

    get token at dashboard route

--- controllers/main.js
+++ controllers/main.js
@@ -19,6 +19,16 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  console.log(token);
+
   const luckyNumber = Math.floor(Math.random() * 100);
   return res.status(200).json({
     msg: `Hello, John Doe`,
